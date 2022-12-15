92ac37c809bbdb76036c881af043427e863e148b
Anastasiya Sachko <95384801+saachko@users.noreply.github.com>
2022-12-15 19:29:08 +0300

    fix: fix problem with imports

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-import { get } from './storage';
-import Keyboard from './Keyboard';
+import { get } from './storage.js';
+import Keyboard from './Keyboard.js';
 
 const rowsOrder = [
   ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'],
